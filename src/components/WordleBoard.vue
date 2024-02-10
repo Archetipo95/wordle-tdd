a886d65212aa7149d19898c68e8de9069209dc7f
Martin Masevski <martin.masevski@gmail.com>
2024-02-10 14:36:37 +0100

    Add defeat message

--- src/components/WordleBoard.vue
+++ src/components/WordleBoard.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { VICTORY_MESSAGE } from '../settings'
+import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '../settings'
 
 defineProps<{
   wordOfTheDay: string
@@ -9,4 +9,5 @@ defineProps<{
 <template>
   <input type="text" name="inputGuess" id="inputGuess" />
   <p>{{ VICTORY_MESSAGE }}</p>
+  <p>{{ DEFEAT_MESSAGE }}</p>
 </template>
--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
-import { VICTORY_MESSAGE } from '../../settings'
+import { VICTORY_MESSAGE, DEFEAT_MESSAGE } from '../../settings'
 
 describe('WordleBoard', () => {
   test('vicotry message appears wheen the user guesses the word', async () => {
@@ -19,4 +19,20 @@ describe('WordleBoard', () => {
     // Assert
     expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
+
+  test("a defeat message appears when the user doesn't guess the word", async () => {
+    const wrapper = mount(WordleBoard, {
+      props: {
+        wordOfTheDay: 'WRONG',
+      },
+    })
+
+    const guessInput = wrapper.find("input[type='text']")
+    await guessInput.setValue('TESTS')
+    await guessInput.trigger('keydown.enter')
+
+    expect(wrapper.text()).toContain(DEFEAT_MESSAGE)
+  })
+
+  test.todo("no end of game message appears when the user hasn't yet made a guess")
 })
